e018f2cd334cb02124545d91be55e02f12df1d9b
hong2goon <hong2goon@email.com>
2024-07-16 10:20:35 +0900

    스타일 수정

--- src/apps/Todo.tsx
+++ src/apps/Todo.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import TodoIcon from '../asset/images/icon/checklist.svg';
 
